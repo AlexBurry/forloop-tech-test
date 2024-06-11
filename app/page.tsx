7abb0a86f0acdec8412ddd6e1c07eab7e41ebf22
AlexBurry <alexjburry@gmail.com>
2024-06-11 19:02:19 +0100

    Restructure pages, add loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,13 @@
-"use client"
-
-import { getData } from "@/app/lib/data";
-import { PokemonList } from "@/app/lib/defintions";
-import { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function Page() {
-  const [data, setData] = useState<PokemonList | null>(null);
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    async function getPokemonAPIData() {
-      const response: PokemonList = await getData(page);
-      setData(response)
-    }
-
-    getPokemonAPIData();
-  }, [page]);
   return (
     <main>
-      <div className="flex-col justify-around ">
-        <h1 className="flex justify-center font-bold text-4xl text-blue-600 p-10">Pokemon!</h1>
-        <div className="flex justify-center border-gray-200 bg-gray-100 p-10 mx-10">
-          <ul className="grid grid-cols-2 gap-x-8 gap-y-4 font-medium ">
-            {data?.results.map((pokemon) => (
-              <li className="py-4 px-6 border border-blue-400 bg-blue-200 min-w-96" key={pokemon.name}>
-                {pokemon.name}
-              </li>
-            ))}
-          </ul> 
-        </div>
-
-        <div className="flex justify-around px-10 py-5">
-          <button onClick={() => setPage(page - 1)} disabled={page ===1} className="bg-blue-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center disabled:opacity-50">
-            Previous Page
-          </button>
-          <button onClick={() => setPage(page + 1)} disabled={!data?.next} className="bg-blue-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center disabled:opacity-50">
-            Next Page
-          </button>
-        </div> 
-      </div>
-        
+      <Link
+      href="/pokemon"
+      className="h-screen flex items-center justify-center">
+        <span className="bg-blue-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">See Pokemon!</span>
+      </Link>
     </main>
   );
 }
---
+++ app/pokemon/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+    return (
+        // <div className="h-screen flex items-center justify-center">
+            <button type="button" className="bg-indigo-500" disabled>
+            <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+            </svg>
+            Loading...
+            </button>
+        // </div>  
+    );
+  }
