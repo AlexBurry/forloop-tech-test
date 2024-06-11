ec59af719ae824ea171f36b2ce5e409299d9c296
AlexBurry <alexjburry@gmail.com>
2024-06-11 17:54:22 +0100

    Add styling

--- app/page.tsx
+++ app/page.tsx
@@ -18,20 +18,28 @@ export default function Page() {
   }, [page]);
   return (
     <main>
-      <h1>Pokemon!</h1>
-      <div>
-        {data?.results.map((pokemon) => (
-          <li key={pokemon.name}>
-            {pokemon.name}
-          </li>
-        ))}
+      <div className="flex-col justify-around ">
+        <h1 className="flex justify-center font-bold text-4xl text-blue-600 p-10">Pokemon!</h1>
+        <div className="flex justify-center border-gray-200 bg-gray-100 p-10 mx-10">
+          <ul className="grid grid-cols-2 gap-x-8 gap-y-4 font-medium ">
+            {data?.results.map((pokemon) => (
+              <li className="py-4 px-6 border border-blue-400 bg-blue-200 min-w-96" key={pokemon.name}>
+                {pokemon.name}
+              </li>
+            ))}
+          </ul> 
+        </div>
+
+        <div className="flex justify-around px-10 py-5">
+          <button onClick={() => setPage(page - 1)} disabled={page ===1} className="bg-blue-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center disabled:opacity-50">
+            Previous Page
+          </button>
+          <button onClick={() => setPage(page + 1)} disabled={!data?.next} className="bg-blue-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center disabled:opacity-50">
+            Next Page
+          </button>
+        </div> 
       </div>
-      <div>
-        <button onClick={() => setPage(page - 1)} disabled={page ===1}>Previous Page</button>
-      </div>
-      <div>
-      <button onClick={() => setPage(page + 1)} disabled={!data?.next}>Next Page</button>
-      </div>     
+        
     </main>
   );
 }
